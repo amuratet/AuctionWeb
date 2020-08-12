349980c8e57945e3a7cfca37ec42acf1affa3ecf
GuYzMoGwAi <blaudlewis@hotmail.fr>
2020-08-12 15:29:23 +0200

    [Lewis] soucie duplicate regle

--- src/fr/eni/AuctionWebapp/DAL/Impl/UtilisateurDaoImpl.java
+++ src/fr/eni/AuctionWebapp/DAL/Impl/UtilisateurDaoImpl.java
@@ -17,21 +17,13 @@ public class UtilisateurDaoImpl implements UtilisateurDao {
 	// =======================================================
 	// CONSTANTES
 	// =======================================================
-<<<<<<< HEAD
-//	private static final String SELECT_ALL = "SELECT u.no_utilisateur,u.pseudo,u.nom,u.prenom,u.email,u.telephone,u.rue,u.code_postal,u.ville,u.credit,u.administrateur FROM UTILISATEURS u;";
-	private static final String SELECT_BY_ID = "SELECT u.no_utilisateur,u.pseudo,u.nom,u.prenom,u.email,u.telephone,u.rue,u.code_postal,u.ville,u.mot_de_passe,u.credit,u.administrateur FROM UTILISATEURS u WHERE u.no_utilisateur = ?;";
-	
-	private static final String SELECT_BY_EMAIL_FOR_CNX = "SELECT no_utilisateur, email, mot_de_passe, administrateur FROM UTILISATEURS WHERE email = ?;";
-	
-=======
 	// private static final String SELECT_ALL = "SELECT
 	// u.no_utilisateur,u.pseudo,u.nom,u.prenom,u.email,u.telephone,u.rue,u.code_postal,u.ville,u.credit,u.administrateur FROM UTILISATEURS u;";
 	private static final String SELECT_BY_ID =
 		"SELECT u.no_utilisateur,u.pseudo,u.nom,u.prenom,u.email,u.telephone,u.rue,u.code_postal,u.ville,u.mot_de_passe,u.credit,u.administrateur FROM UTILISATEURS u WHERE u.no_utilisateur = ?;";
 
-	private static final String SELECT_BY_EMAIL_FOR_CNX = "SELECT no_utilisateur, email, mot_de_passe FROM UTILISATEURS WHERE email = ?;";
+	private static final String SELECT_BY_EMAIL_FOR_CNX = "SELECT no_utilisateur, email, mot_de_passe, administrateur FROM UTILISATEURS WHERE email = ?;";
 
->>>>>>> 5284238fed9b7f1d651cdb9aad5f67ffcffd2998
 	private static final String INSERT_UTILISATEUR = "INSERT INTO UTILISATEURS (pseudo,nom,prenom,email,telephone,rue,code_postal,ville,mot_de_passe,credit,administrateur)"
 		+ "VALUES(?,?,?,?,?,?,?,?,?,0,0)";
 
@@ -94,16 +86,7 @@ public class UtilisateurDaoImpl implements UtilisateurDao {
 
 			res = req.executeQuery();
 			if (res.next()) {
-<<<<<<< HEAD
-				utilisateur = new Utilisateur(
-						res.getInt("no_utilisateur"),
-						res.getString("email"),
-						res.getString("mot_de_passe"),
-						res.getBoolean("administrateur")
-						);
-=======
-				utilisateur = new Utilisateur(res.getInt("no_utilisateur"), res.getString("email"), res.getString("mot_de_passe"));
->>>>>>> 5284238fed9b7f1d651cdb9aad5f67ffcffd2998
+				utilisateur = new Utilisateur(res.getInt("no_utilisateur"), res.getString("email"), res.getString("mot_de_passe"), res.getBoolean("administrateur"));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -159,8 +142,8 @@ public class UtilisateurDaoImpl implements UtilisateurDao {
 	@Override
 	public void update(Utilisateur utilisateur) throws DALException {
 		// Utilisateur utilisateurUpdate = utilisateur.getPseudo();
-//		PreparedStatement req = null;
-//		ResultSet res = null;
+		// PreparedStatement req = null;
+		// ResultSet res = null;
 
 		// try (Connection cnx = FournisseurConnexion.obtenirConnexion()) {
 		// req = cnx.prepareStatement(UPDATE_UTILISATEUR);
